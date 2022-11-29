5e85dc54750e0a7e05b2699a2ec9d91da01010a6
Desenvolvimento <guleandro.66#gmail.com>
2022-11-29 02:56:17 -0300

    create crud model bike

--- src/controllers/ModelBikeController.ts
+++ src/controllers/ModelBikeController.ts
@@ -1,13 +1,68 @@
 import { Request, Response } from "express";
+import { ModelBikeUseCase } from "../usercase/ModelBikeUseCase";
 
 class ModelBikeController {
-  async store(request: Request, response: Response) {}
+  async store(request: Request, response: Response) {
+    const { nameModel } = request.body;
 
-  async update(request: Request, response: Response) {}
+    if (!nameModel) {
+      return response.status(500).json({ error: "Field is missing" });
+    }
 
-  async show(request: Request, response: Response) {}
+    const modelBike = new ModelBikeUseCase();
+    const result = modelBike.save();
 
-  async list(request: Request, response: Response) {}
+    if (result instanceof Error) {
+      return response.status(500).json({ error: result.message });
+    }
+
+    return response.status(201).json(result);
+  }
+
+  async update(request: Request, response: Response) {
+    const { nameModel } = request.body;
+
+    if (!nameModel) {
+      return response.status(500).json({ error: "Field is missing" });
+    }
+
+    const modelBike = new ModelBikeUseCase();
+    const result = modelBike.update();
+
+    if (result instanceof Error) {
+      return response.status(500).json({ error: result.message });
+    }
+
+    return response.status(201).json(result);
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.body;
+
+    if (!id) {
+      return response.status(500).json({ error: "Field is missing" });
+    }
+
+    const modelBike = new ModelBikeUseCase();
+    const result = modelBike.show();
+
+    if (result instanceof Error) {
+      return response.status(500).json({ error: result.message });
+    }
+
+    return response.status(201).json(result);
+  }
+
+  async list(request: Request, response: Response) {
+    const modelBike = new ModelBikeUseCase();
+    const result = modelBike.list();
+
+    if (result instanceof Error) {
+      return response.status(500).json({ error: result.message });
+    }
+
+    return response.status(201).json(result);
+  }
 }
 
 export { ModelBikeController };
--- src/usercase/ModelBikeUseCase.ts
+++ src/usercase/ModelBikeUseCase.ts
@@ -1,8 +1,74 @@
+import { ModelBike } from "../models/ModelBike";
+import { getRepositoryModelBike } from "../repository/ModelBikeRepository";
+
 class ModelBikeUseCase {
   async save() {}
-  async update() {}
-  async show() {}
-  async list() {}
+
+  async update(id: string, nameModelBike: string) {
+    const modelbike = await getRepositoryModelBike.findOneBy({
+      id: id,
+    });
+
+    if (!modelbike) {
+      return new Error("Model Bike not found");
+    }
+
+    try {
+      const result = await getRepositoryModelBike
+        .createQueryBuilder()
+        .update(ModelBike)
+        .set({
+          nameModel: nameModelBike
+        })
+        .where("id :id", { id: id })
+        .execute();
+
+      if (result.affected != 1) {
+        return new Error("Error when updating");
+      }
+
+      const modelBike = await getRepositoryModelBike.findOneBy({
+        id: id,
+      });
+
+      return modelBike;
+    } catch (error) {
+      console.log(error);
+      return new Error("Model bikes not found");
+    }
+  }
+
+  async show(id: string) {
+    try {
+      const modelBike = await getRepositoryModelBike.findOneBy({
+        id: id,
+      });
+
+      if (!modelBike) {
+        return new Error("Model Bike not found");
+      }
+
+      return modelBike;
+    } catch (error) {
+      console.log(error);
+      return new Error("Model bikes not found");
+    }
+  }
+
+  async list() {
+    try {
+      const list = await getRepositoryModelBike.find();
+
+      if (!list) {
+        return new Error("Model Bike not found");
+      }
+
+      return list;
+    } catch (error) {
+      console.log(error);
+      return new Error("Models bikes not found");
+    }
+  }
 }
 
 export { ModelBikeUseCase };
