98d1695b2aac045ea03db7e98226a24065948627
Desenvolvimento <guleandro.66#gmail.com>
2022-12-02 19:45:41 -0300

    adjust consumer User

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import { routes } from "../router/routes"
+import { routes } from "../router/routes";
 import { AdminUserUseCase } from "../usercase/AdminUserUseCase";
 import { RabbitMQServer } from "./RabbitMQServer";
 
@@ -17,15 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 const serveramqp = async () => {
-    const server = new RabbitMQServer();
-    await server.start();
-    await server.consume("bike.admin", (message) => {
-      const infoUser = message.content.toString();
-      const userCase = new AdminUserUseCase();
-      userCase.execute(infoUser);
-    });
-  };
-  
-  serveramqp();
+  const server = new RabbitMQServer();
+  await server.start();
+  await server.consume("bike.admin", (message) => {
+    const infoUser = message.content.toString();
+    const userCase = new AdminUserUseCase();
+    userCase.execute(infoUser);
+  });
+};
+
+serveramqp();
 
 export { app, httpServer };
