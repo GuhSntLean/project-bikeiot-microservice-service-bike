c89df352180026d9a4e14ad459a83d032c4ad0ac
Desenvolvimento <guleandro.66#gmail.com>
2022-12-03 02:23:35 -0300

    move RabbitMQ consumer to server after start server

--- src/server/app.ts
+++ src/server/app.ts
@@ -16,16 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
 
-const serveramqp = async () => {
-  const server = new RabbitMQServer();
-  await server.start();
-  await server.consume("bike.admin", (message) => {
-    const infoUser = message.content.toString();
-    const userCase = new AdminUserUseCase();
-    userCase.execute(infoUser);
-  });
-};
+// const serveramqp = async () => {
+//   const server = new RabbitMQServer();
+//   await server.start();
+//   await server.consume("bike.admin", (message) => {
+//     const infoUser = message.content.toString();
+//     const userCase = new AdminUserUseCase();
+//     userCase.execute(infoUser);
+//   });
+// };
 
-serveramqp();
+// serveramqp();
 
 export { app, httpServer };
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,19 @@
 import { app, httpServer } from "./app";
 import os from "os";
 import { AppDataSource } from "../config/AppDataSource";
+import { RabbitMQServer } from "./RabbitMQServer";
+import { AdminUserUseCase } from "../usercase/AdminUserUseCase";
+
+const serveramqp = async () => {
+  const server = new RabbitMQServer();
+  await server.start();
+  await server.consume("bike.admin", (message) => {
+    const infoUser = message.content.toString();
+    const userCase = new AdminUserUseCase();
+    userCase.execute(infoUser);
+  });
+};
+
 
 const server = async () => {
   try {
@@ -17,3 +30,4 @@ const server = async () => {
 };
 
 server();
+serveramqp();
